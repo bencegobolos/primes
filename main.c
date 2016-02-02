abc5c815f0600a14590680cc15095148cbf49169
Bence Gobolos <bence.gobolos@hightec-rt.com>
2016-02-02 16:14:56 +0100

    Improved algorithm and code styling.

--- main.c
+++ main.c
@@ -1,49 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
 #define TRUE 1
 #define FALSE 0
+#define ERROR "Error: "
+
+/*
+ * This program is for finding the N-th prime number.
+ * Waits for one argument: N (unsigned int).
+ * Returns the N-th prime.
+ */
 
 int main(int argc, char* argv[])
 {
+  // Input handling.
   if (argc != 2) {
-    printf("One argument: unsigned int!");
+    printf("%sOne argument: unsigned int!\n", ERROR);
     return -1;
   }
 
-  clock_t start = clock();
-
+  // Input parameter.
   unsigned int param = atoi(argv[1]);
 
+  // Early return when param is 1.
   if (param == 1) {
-    printf("The 1. prime is: 2");
+    printf("The 1. prime is: 2\n");
     return 0;
   }
 
-  if (param == 2) {
-    printf("The 2. prime is: 3");
-    return 0;
-  }
+  // Start measuring time.
+  clock_t start = clock();
 
-  unsigned int counter = 1;
-  unsigned int number = 3;
-  unsigned int i;
-  short int isprime = TRUE;
+  // Array to store prime numbers.
   unsigned int* primes = malloc(param * sizeof(int));
+  // Is the number a prime?
+  short isprime = TRUE;
+  // Loop variable.
+  unsigned int i;
+  // How many primes we have found.
+  unsigned int counter = 2;
+  // The number that is being checked.
+  unsigned int number = 3;
+  // The number's half. It is for optimization purpose.
+  unsigned int number_half;
 
-  if (param <= 2) {
-    return -2;
-  }
+  // Primes' array initialization.
+  primes[0] = 2;
+  primes[1] = 3;
 
-  primes[0] = 3;
-
-  while (counter != param) {
+  while (counter < param) {
     number += 2;
+    number_half = number/2;
     isprime = TRUE;
 
-    for (i = 0; i < counter; i++) {
-      if (number % primes[i] == 0) {
+    for (i = 1; i < counter; i++) {
+      if (primes[i] > number_half)
+        break;
+
+      if (!(number % primes[i])) {
         isprime = FALSE;
         break;
       }
@@ -55,9 +71,10 @@ int main(int argc, char* argv[])
     }
   }
 
-  printf("The %u prime is: %u\n", param, number);
+  printf("The %u. prime is: %u\n", param, number);
   free(primes);
 
+  // Stop measuring time.
   clock_t end = clock();
   double secs = (double)(end - start) / CLOCKS_PER_SEC;
   printf("Elapsed secs: %f\n", secs);
